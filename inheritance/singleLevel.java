4e57df5ec4c792bda8b9ccdbaa26f7cd1eceb4b9
Keshav Gupta <gupta.keshav0406@gmail.com>
2026-04-09 01:37:05 +0530

    inheritance

--- inheritance/singleLevel.java
+++ inheritance/singleLevel.java
@@ -5,8 +5,14 @@ class animal{
 }
 
 class Dog extends animal{   //dog class inheriting properties of animal class
-    public void bark(){
+    public void eat(){   // this is method overriding -> run time polymorphism as method behavior is changing
         System.out.println("barking...");
+    }
+}
+
+class cat extends animal{   //cat class inheriting properties of animal class
+    public void eat(int x){ // this is method overloading -> compile time polymorphism same method name but different parameters
+        System.out.println("eating " + x + " units...");
     }
 }
 
